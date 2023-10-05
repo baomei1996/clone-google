e1af4b4c7cc0d34bcaabd1115754e746b61a3f03
baomei1996 <qhal4686@gmail.com>
2023-10-05 21:20:12 +0900

    Feat: Fetch data from google search API and show the titles

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,5 +1,28 @@
 import React from "react";
 
-export default function WebSearchPage() {
-    return <div>WebSearchPage</div>;
+type SearchResultType = {
+    title: string;
+    link: string;
+    snippet: string;
+    formattedUrl: string;
+};
+
+export default async function WebSearchPage({
+    searchParams,
+}: {
+    searchParams: { searchTerm: string };
+}) {
+    const response = await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+    );
+    const data = await response.json();
+    const results: SearchResultType[] = data.items;
+
+    return (
+        <div>
+            {results.map((result, index) => (
+                <h1 key={index}>{result.title}</h1>
+            ))}
+        </div>
+    );
 }
