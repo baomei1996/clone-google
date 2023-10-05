ab872c038151138b560a3b0d7449d6db1a99a125
baomei1996 <qhal4686@gmail.com>
2023-10-05 21:40:00 +0900

    Feat: Handle possible errors

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 type SearchResultType = {
@@ -17,6 +18,20 @@ export default async function WebSearchPage({
     );
     const data = await response.json();
     const results: SearchResultType[] = data.items;
+
+    if (!results) {
+        return (
+            <div className="flex flex-col justify-center items-center pt-10">
+                <h1 className="text-3xl pb-4">No results found.</h1>
+                <p className="text-lg">
+                    Try search for something else or go back to the homepage.
+                    <Link href={"/"} className="text-blue-500 ml-2">
+                        HOME
+                    </Link>
+                </p>
+            </div>
+        );
+    }
 
     return (
         <div>
