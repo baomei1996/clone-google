805640f106f4130eaf4f1f15ec45b99312764b6a
baomei1996 <qhal4686@gmail.com>
2023-10-05 14:48:35 +0900

    Feat: Add footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,11 @@ export default function RootLayout({
     return (
         <html lang="en">
             <link rel="icon" href="./favicon.svg" />
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                {children}
+                {/* footer */}
+                <Footer />
+            </body>
         </html>
     );
 }
